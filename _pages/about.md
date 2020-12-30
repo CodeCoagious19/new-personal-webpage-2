5a6d2d9b07060d2ff5dcfbd46475b2404992f82a
sidiri-8bit <simone.diricco.1@gmail.com>
2020-12-30 09:45:10 +0100

    add initial layout

---
+++ _pages/about.md
@@ -0,0 +1,9 @@
+---
+layout: single
+title: "About"
+permalink: /pages/about/
+excerpt: "chi sono"
+last_modified_at: 2020-07-27
+---
+
+# Ciao
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,37 @@
-var store = []
+var store = [{
+        "title": "Lezione 01",
+        "excerpt":"Prerequisiti Conoscenza di un linguaggio qualsiasi OOP come C++ o Javascript Corso Python Python è un linguaggio di programmazione di più “alto livello” rispetto alla maggior parte degli altri linguaggi, orientato a oggetti, adatto, tra gli altri usi, a sviluppare applicazioni distribuite, scripting, computazione numerica e system testing. Ideato da...","categories": [],
+        "tags": [],
+        "url": "/python/lezione-01",
+        "teaser": null
+      },{
+        "title": "Lezione 02",
+        "excerpt":"Primo script Ogni file python ha un’estensione .py. Per l’esecuzione di uno script python è possibile lanciare il file creato con l’estensione .py (esempio test.py) direttamente dal terminale digitando il comando per l’invocazione dell’interprete python python3 ad esempio python3 test.py all’interno della cartella contenente il file test.py oppure attraverso l’estensione...","categories": [],
+        "tags": [],
+        "url": "/python/lezione-02",
+        "teaser": null
+      },{
+        "title": "Lezione 03",
+        "excerpt":"Introduzione al modulo PyPDF2 Il seguente script tratterà l’elaborazione di file pdf. Saremo in grado quindi di generare un file pdf con nome predefinito “Output.pdf” che è la copia della prima pagina di un pdf con nome “input.pdf” ricevuto in input Faremo uso del modulo aggiuntivo PyPDF2 da installare da...","categories": [],
+        "tags": [],
+        "url": "/python/lezione-03",
+        "teaser": null
+      },{
+        "title": "Lezione 04",
+        "excerpt":"Costrutto condizionale if...else Le istruzioni condizionali vengono utilizzate per eseguire un blocco di codice solo nel caso in cui una condizione risulti true o false Sintassi costrutto if esteso: if condition1: #code 01 #.. elif condition2: #code 02 #.. else: #code 03 #.. Esempio: print(\"Inserisci due numeri..\") primoValore = input()...","categories": [],
+        "tags": [],
+        "url": "/python/lezione-04",
+        "teaser": null
+      },{
+        "title": "Lezione 05",
+        "excerpt":"PyPDF2 - Copia PDF Il seguente script prende in input un pdf “input.pdf” e ne produce una copia sul file “Output.pdf” Faremo uso della proprietà della classe PdfFileReader: numPages: Restituisce il numero delle pagine del file pdf dell’istanza (della classe PdfFileReader) si cui si legge la proprietà import PyPDF2 pdfInput...","categories": [],
+        "tags": [],
+        "url": "/python/lezione-05",
+        "teaser": null
+      },{
+        "title": "Lezione 06",
+        "excerpt":"Liste Abbiamo già visto implicitamente un’introduzione al tipo lista e in particolare come creare e iterare sugli elementi di una lista attraverso il ciclo for e la funzione range(). Il tipo dato lista è molto importante nel linguaggio python. Le liste sono un tipo di dato utilizzato per memorizzare diversi...","categories": [],
+        "tags": [],
+        "url": "/python/lezione-06",
+        "teaser": null
+      }]
